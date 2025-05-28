bc10433e6b39070b58c5f8471a1b686dc340219d
Yash Devani <yashdewani159@gmail.com>
2025-05-28 13:27:03 +0530

    backend playlist controller getPlayListDetails done

--- Backend/src/controllers/playlist.controller.js
+++ Backend/src/controllers/playlist.controller.js
@@ -58,7 +58,43 @@ export const getAllListDetails = (req, res) => {
     }
 }
 
-export const getPlayListDetails = (req, res) => {}
+export const getPlayListDetails = (req, res) => {
+    const {playlistId} = req.params;
+    try {
+        const playlist = db.playlist.findUnique({
+            where:{
+                id:playlistId,
+                userId:req.user.userId
+            },
+            include:{
+                problems:{
+                    include:{
+                        problem:true
+                    }
+                }
+            }
+        })
+        if(!playlist){
+            return res.status(404).json({
+                success: false,
+                error: "Playlist not found"
+            })
+        }
+       return res.status(200).json({
+            success: true,
+            message: "Playlist fetched successfully",
+            playlist:playlist
+        })
+
+    } catch (error) {
+        console.log("Error fetching playlist", error);
+        res.status(500).json({
+            success: false,
+            error: "Error fetching playlist"
+        })
+        
+    }
+}
 
 export const addProblemToPlaylist = (req, res) => {}
 
